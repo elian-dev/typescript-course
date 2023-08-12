e780561a6be168885d9ef4137cd8ef854c463c7d
elian_dev <geordymemdoza@gmail.com>
2023-08-11 23:51:15 -0600

    feat: :memo: Using union types - Update

--- src/09-union-types.ts
+++ src/09-union-types.ts
@@ -1,19 +1,21 @@
 (() => {
   let userId: string | number; // Now you are able to assing a string or a number to this var, any other type is wrong
   userId = 123;
-  userId = 'asd';
+  userId = '12345';
 
   console.log(userId)
 
   function greeting(param: string | number ) { // In this function you can receive an argument rather string or number
     if(typeof param === 'string') {
-      param.toLocaleLowerCase() // Thanks to typeof of JS, wen can use the correct properties for strings in this scope
+      // Thanks to typeof of JS, wen can use the correct properties for strings in this scope
+      console.log(`String: ${param.toLocaleLowerCase()}`)
     } else {
-      param.toFixed() // Thanks to typeof of JS, wen can use the correct properties for numbers in this scope
+      // Thanks to typeof of JS, wen can use the correct properties for numbers in this scope
+      console.log(`Number: ${param.toFixed(1)}`)
     }
   }
 
   greeting('Hola!');
-  greeting(1.4);
+  greeting(12.4444);
 
-})
+})();
